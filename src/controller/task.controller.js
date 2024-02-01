5354ef67eeff0125e62dde18b73b0884711db973
a.falei <august3@mail.ru>
2024-02-01 18:29:22 +0300

    buildResponse

--- src/controller/task.controller.js
+++ src/controller/task.controller.js
@@ -1,48 +1,47 @@
 const express = require('express');
+const { buildResponse } = require('../helper/respons');
 const { createTask, updateTask, deleteTask, getByIdTask, getAllTask } = require('../service/task.service');
 const route = express.Router();
 
 route.get('/', async (req, res) => {
-  try {
-    const data = await getAllTask();
-    res.send(data);
-  } catch (error) {
-    res.send(error.message);
-  }
+    try {
+        buildResponse(res, 200, await getAllTask());
+    } catch (error) {
+        buildResponse(res, 404, error.message);
+    }
 });
 route.get('/:_id', async (req, res) => {
-  try {
-    const data = await getByIdTask(req.params._id);
-    res.send(data);
-  } catch (error) {
-    res.send(error.message);
-  }
+    try {
+        buildResponse(res, 200, await getByIdTask(req.params._id));
+    } catch (error) {
+        buildResponse(res, 404, error.message);
+    }
 });
 
 route.post('/', async (req, res) => {
-  try {
-    const task = req.body;
-    res.send(await createTask(task));
-  } catch (error) {
-    res.send(error.message);
-  }
+    try {
+        const task = req.body;
+        buildResponse(res, 200, await createTask(task));
+    } catch (error) {
+        buildResponse(res, 404, error.message);
+    }
 });
 
 route.put('/:_id', async (req, res) => {
-  try {
-    const task = req.body;
-    res.send(await updateTask(req.params._id, task));
-  } catch (error) {
-    res.send(error.message);
-  }
+    try {
+        const task = req.body;
+        buildResponse(res, 200, await updateTask(req.params._id, task));
+    } catch (error) {
+        buildResponse(res, 404, error.message);
+    }
 });
 
 route.delete('/:_id', async (req, res) => {
-  try {
-    res.send(await deleteTask(req.params._id));
-  } catch (error) {
-    res.send(error.message);
-  }
+    try {
+        buildResponse(res, 200, await deleteTask(req.params._id));
+    } catch (error) {
+        buildResponse(res, 404, error.message);
+    }
 });
 
 module.exports = route;
