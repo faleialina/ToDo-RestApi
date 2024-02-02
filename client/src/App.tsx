496afcab3b10afd0c0d8dd5984c0861b0dff3b0a
a.falei <august3@mail.ru>
2024-02-02 22:35:28 +0300

    add client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import Main from "./Main/Main";
+
+export default function App() {
+
+  return (
+  <>
+  <Main/>
+  </>
+
+  )
+}
+
+
---
+++ client/src/Main/Main.tsx
@@ -0,0 +1,16 @@
+import style from './style.module.scss'
+
+export default function Main() {
+
+    return (
+        <div className={style.wrapper}>
+            <h1>TODO LIST</h1>
+            <div className={style.header}>
+                <input type="text" placeholder='enter note...' />
+                <input type="text" placeholder='enter description...' />
+                <button>CREATE</button>
+            </div>
+        </div>
+    )
+}
+
