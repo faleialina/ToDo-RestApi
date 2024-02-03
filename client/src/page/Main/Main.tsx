83b075d004c412a512bbbd7cdc6f4c274ca38002
a.falei <august3@mail.ru>
2024-02-03 14:32:22 +0300

    add cors and axios

--- client/src/page/Main/Main.tsx
+++ client/src/page/Main/Main.tsx
@@ -1,14 +1,19 @@
+import { useState } from 'react'
 import style from './style.module.scss'
 
 export default function Main() {
+    const [task, setTask] = useState({ title: '', description: '' });
+    function changeInput(e) {
+        setTask({ ...task, [e.target.name]: e.target.value });
+    };
 
     return (
         <div className={style.wrapper}>
             <h1>TODO LIST</h1>
             <div className={style.header}>
-                <input type="text" placeholder='enter note...' />
-                <input type="text" placeholder='enter description...' />
-                <button>CREATE</button>
+                <input type="text" name='title' placeholder='enter title...' onChange={changeInput}/>
+                <input type="text" name='description' placeholder='enter description...'onChange={changeInput} />
+                <button type="submit">CREATE</button>
             </div>
         </div>
     )
--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const task = require('./controller/task.controller');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('../swagger.json');
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/task', task);
